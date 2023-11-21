88b960145f4851bb8422725caa92a4f184e02479
ManishJoc14 <manishjoc14@gmail.com>
2023-11-21 19:44:12 +0545

    sass added

---
+++ frontend/src/components/HomePage/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './HomePage.module.scss'
+const index = () => {
+  return (
+    <div className={styles.main}>
+        hello
+    </div>
+  )
+}
+
+export default index
