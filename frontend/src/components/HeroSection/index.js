f7dd4e87e02d3b65ba3b22655fbc13584da9bf6e
bibekjoshi01 <bibekjoshi34@gmail.com>
2023-11-22 12:27:36 +0545

    hero section dynamic css

--- frontend/src/components/HeroSection/index.js
+++ frontend/src/components/HeroSection/index.js
@@ -1,28 +1,35 @@
 import React from "react";
 import banner from "../../assets/banner.jpg";
 import styles from "./HeroSection.module.scss";
+import { FaVideo } from "react-icons/fa6";
 
 const index = () => {
   return (
-    <>
-      <section className={styles.main}>
-        <div className={styles.container}>
-          <div className={styles.left}>
-            <h1>ZINGO: Chat with anyone & anywhere.</h1>
-            <p className={styles.description}>
+    <section className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <h1>
+            <span className={styles.title}>ZINGO:</span> Chat with anyone &
+            anywhere.
+          </h1>
+          <div className={styles.description}>
+            <p>
               Say goodbye to limitations and hello to a new era of universal
               connectivity. Join the revolution now!
             </p>
+          </div>
+          <div className={styles.btn}>
             <a href=" " className={styles.talkBtn}>
-              Talk To Strangers
+              <FaVideo className={styles.icon} />{" "}
+              <span className={styles.btnText}>Talk To Strangers</span>{" "}
             </a>
           </div>
-          <div className={styles.right}>
-            <img src={banner} alt="Banner" className={styles.banner} />
-          </div>
         </div>
-      </section>
-    </>
+        <div className={styles.right}>
+          <img src={banner} alt="Banner" className={styles.banner} />
+        </div>
+      </div>
+    </section>
   );
 };
 
