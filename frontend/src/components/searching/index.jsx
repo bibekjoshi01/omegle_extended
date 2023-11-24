c8e8a960db84e641f6276b64eb95c5099f582de8
bibekjoshi01 <bibekjoshi34@gmail.com>
2023-11-24 20:51:51 +0545

    -

--- frontend/src/components/searching/index.jsx
+++ frontend/src/components/searching/index.jsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import styles from './Searching.module.scss';
-import { useDispatch } from 'react-redux';
-import { createStarter } from '../Starter/redux/thunk';
+import React from "react";
+import styles from "./Searching.module.scss";
+import { useDispatch } from "react-redux";
+import { createStarter } from "../Starter/redux/thunk";
+import { RiUserSearchLine } from "react-icons/ri";
+
 const Searching = () => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const usersData = JSON.parse(localStorage.getItem("userData"));
 
-	const handleSearch = () => {
-		const usersData = JSON.parse(localStorage.getItem('userData'));
-
-		const value = {
-			user_id: Math.floor(Math.random() * 10),
-			nickname: usersData.nickName,
-			gender: usersData.usersGender.toUpperCase(),
-			interested_gender: usersData.interestedGender.toUpperCase(),
-		};
-		dispatch(createStarter(value));
-	};
+  const handleSearch = () => {
+    const value = {
+      user_id: Math.floor(Math.random() * 10),
+      nickname: usersData.nickName,
+      gender: usersData.usersGender.toUpperCase(),
+      interested_gender: usersData.interestedGender.toUpperCase(),
+    };
+    dispatch(createStarter(value));
+  };
   return (
     <div className={styles.main}>
-      <p className={styles.usersName}>Hii {userData?.nickName} !! </p>
+      <p className={styles.usersName}>Hii {usersData?.nickName} !! </p>
       <button onClick={handleSearch} className={styles.searchingPage}>
         Start Searching <RiUserSearchLine />
       </button>
     </div>
   );
-
+};
 export default Searching;
