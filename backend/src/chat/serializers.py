91aeba36ae8e5af0ef3c9c658e48e83f74c98842
bibekjoshi01 <bibekjoshi34@gmail.com>
2023-12-09 14:04:40 +0545

    backend api changed

--- backend/src/chat/serializers.py
+++ backend/src/chat/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
-from . models import ChatRoom, ChatMessage
+from .models import ChatRoom, ChatMessage
 
 GENDERS = (
-    ('MALE', 'Male'),
-    ('FEMALE', 'Female'),
-    ('OTHER', 'Other'),
+    ("MALE", "Male"),
+    ("FEMALE", "Female"),
+    ("OTHER", "Other"),
 )
 
+
 class InitializeChatSerializer(serializers.Serializer):
-    user_id = serializers.CharField(max_length=20)
     nickname = serializers.CharField(max_length=20)
     gender = serializers.ChoiceField(choices=GENDERS)
     interested_gender = serializers.ChoiceField(choices=GENDERS)
@@ -17,25 +17,61 @@ class InitializeChatSerializer(serializers.Serializer):
 class ChatMessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ChatMessage
-        fields = ['initiator', 'message', 'timestamp']
+        fields = ["id", "initiator", "message", "timestamp"]
 
 
 class ChatRoomSerializer(serializers.ModelSerializer):
-    messages = ChatMessageSerializer(many=True, source='chat_messages', read_only=True)
+    messages = ChatMessageSerializer(many=True, source="chat_messages", read_only=True)
 
     class Meta:
         model = ChatRoom
-        fields = ['room_id', 'messages']
+        fields = ["room_id", "messages"]
 
 
 class SendChatMessageSerializer(serializers.Serializer):
     room_id = serializers.UUIDField()
-    initiator = serializers.CharField(max_length=20)
+    initiator_id = serializers.CharField(max_length=20)
     message = serializers.CharField(max_length=255)
 
 
 class ChatRoomInfoSerializer(serializers.ModelSerializer):
+    member1_nickname = serializers.SerializerMethodField()
+    member1_id = serializers.SerializerMethodField()
+    member2_nickname = serializers.SerializerMethodField()
+    member2_id = serializers.SerializerMethodField()
+    started_at = serializers.DateTimeField(source="created_at")
+
     class Meta:
         model = ChatRoom
-        fields = ['room_id', 'name', 'member1', 'member2', 'status']
-        
+        fields = [
+            "room_id",
+            "name",
+            "member1_nickname",
+            "member1_id",
+            "member2_nickname",
+            "member2_id",
+            "status",
+            "started_at",
+        ]
+
+    def get_member_name(self, user_id):
+        # Assuming the user_id is in the format "Name-some_id"
+        # Split the string and return the first part as the name
+        return user_id.split("-")[0] if user_id else None
+
+    def get_member_id(self, user_id):
+        # Assuming the user_id is in the format "Name-some_id"
+        # Split the string and return the second part as the name
+        return user_id.split("-")[1] if user_id else None
+
+    def get_member1_nickname(self, obj):
+        return self.get_member_name(obj.member1)
+
+    def get_member2_nickname(self, obj):
+        return self.get_member_name(obj.member2)
+
+    def get_member1_id(self, obj):
+        return self.get_member_id(obj.member1)
+
+    def get_member2_id(self, obj):
+        return self.get_member_id(obj.member2)
--- backend/src/chat/views.py
+++ backend/src/chat/views.py
@@ -8,10 +8,15 @@ from rest_framework.permissions import AllowAny
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import ValidationError
 
-from . serializers import (
-    InitializeChatSerializer, SendChatMessageSerializer, ChatRoomSerializer, ChatRoomInfoSerializer
+from src.chat.utils import generate_unique_user_id
+
+from .serializers import (
+    InitializeChatSerializer,
+    SendChatMessageSerializer,
+    ChatRoomSerializer,
+    ChatRoomInfoSerializer,
 )
-from . models import ChatRoom, ChatMessage
+from .models import ChatRoom, ChatMessage
 
 
 class InitializeChatAPIView(APIView):
@@ -21,41 +26,47 @@ class InitializeChatAPIView(APIView):
     @transaction.atomic
     def post(self, request):
         serializer = self.serializer_class(
-            data=request.data, context={'request': request}
+            data=request.data, context={"request": request}
         )
 
         if serializer.is_valid(raise_exception=True):
             data = serializer.validated_data
-            
-            user_id = data['user_id']
-            nickname = data['nickname']
+
+            user_id = generate_unique_user_id()
+            nickname = data["nickname"]
 
             # Matching Process will be here
-            member1 = user_id
+            member_detail = "{}-{}".format(nickname, user_id)
 
-           # Check if there's a user waiting for a match
-            waiting_user = ChatRoom.objects.filter(member2=None,status="WAITING").first()
+            # Check if there's a user waiting for a match
+            waiting_user = ChatRoom.objects.filter(
+                member2=None, status="WAITING"
+            ).first()
 
             if waiting_user:
                 # Pair the waiting user with the current user
                 chat_room = waiting_user
-                chat_room.member2 = user_id
-                chat_room.status = 'MATCHED'
+                chat_room.member2 = member_detail
+                chat_room.status = "MATCHED"
                 chat_room.save()
             else:
+                room_name = nickname + "'s Room"
                 # No waiting user found, create a new chat room for the current user
-                chat_room = ChatRoom.objects.create(name=nickname, member1=user_id)
-                chat_room.status = 'WAITING'
+                chat_room = ChatRoom.objects.create(
+                    name=room_name, member1=member_detail
+                )
+                chat_room.status = "WAITING"
                 chat_room.save()
 
             return Response(
                 {
-                    'message': 'New Chat Initialized Successfully !',
-                    'status': chat_room.status,
-                    'user_id': user_id,
-                    'room_id': chat_room.room_id
+                    "message": "New Chat Initialized Successfully !",
+                    "status": chat_room.status,
+                    "user_id": user_id,
+                    "nickname": nickname,
+                    "room_id": chat_room.room_id,
                 },
-                status=status.HTTP_201_CREATED
+                status=status.HTTP_201_CREATED,
             )
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -67,9 +78,9 @@ class CheckChatStatusAPIView(APIView):
     def get(self, request, room_id):
         try:
             chat_room = ChatRoom.objects.get(room_id=room_id)
-            return Response({'status': chat_room.status}, status=status.HTTP_200_OK)
+            return Response({"status": chat_room.status}, status=status.HTTP_200_OK)
         except ChatRoom.DoesNotExist:
-            return Response({'status': 'not found'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"status": "not found"}, status=status.HTTP_404_NOT_FOUND)
 
 
 class ChatRoomAPIView(APIView):
@@ -82,41 +93,64 @@ class ChatRoomAPIView(APIView):
 
             return Response(serializer.data, status=status.HTTP_200_OK)
         except ChatRoom.DoesNotExist:
-            return Response({'message': 'not found'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"message": "not found"}, status=status.HTTP_404_NOT_FOUND)
 
 
-class SendChatMessageAPIView(generics.CreateAPIView):
+class SendChatMessageAPIView(APIView):
     permission_classes = [AllowAny]
     serializer_class = SendChatMessageSerializer
 
-    def perform_create(self, serializer):
-        room_id = serializer.validated_data['room_id']
-        initiator = serializer.validated_data['initiator']
+    def get_member_id(self, user_id):
+        return user_id.split("-")[1] if user_id else None
 
-        # Check if the initiator belongs to the specified chat room
-        try:
-            chat_room = ChatRoom.objects.get(room_id=room_id)
-            if initiator != chat_room.member1 and initiator != chat_room.member2:
+    def post(self, request):
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
+        if serializer.is_valid(raise_exception=True):
+            validated_data = serializer.validated_data
+
+            room_id = validated_data["room_id"]
+            initiator_id = validated_data["initiator_id"]
+
+            try:
+                chat_room = ChatRoom.objects.get(room_id=room_id)
+            except ChatRoom.DoesNotExist:
                 return Response(
-                    {'message': 'Initiator does not belong to this chat room.'}, 
-                    status=status.HTTP_BAD_REQUEST
+                    {"message": "Chat room not found."}, status=status.HTTP_NOT_FOUND
                 )
-        except ChatRoom.DoesNotExist:
-            return Response(
-                {'message': 'Chat room not found.'}, 
-                status=status.HTTP_NOT_FOUND
+
+            # Check if the initiator belongs to the specified chat room
+            try:
+                member1_id = self.get_member_id(chat_room.member1)
+                member2_id = self.get_member_id(chat_room.member2)
+            except (AttributeError, IndexError):
+                return Response(
+                    {"message": "Invalid format for initiator or member ID."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            if initiator_id not in [member1_id, member2_id]:
+                return Response(
+                    {"message": "Initiator does not belong to this chat room."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            # Create the chat message within the chat room
+            chat_message = ChatMessage.objects.create(
+                chat=chat_room,
+                initiator=initiator_id,
+                message=validated_data["message"],
             )
-        
-       # Create the chat message within the chat room
-        chat_message = ChatMessage.objects.create(
-            chat=chat_room, initiator=initiator, 
-            message=serializer.validated_data['message']
-        )
-        chat_message.save()
-        return Response(
-            {'message': 'Message Sent Successfully.'}, 
-            status=status.HTTP_201_CREATED
-        )
+            chat_message.save()
+
+            return Response(
+                {"message": "Message Sent Successfully."},
+                status=status.HTTP_201_CREATED,
+            )
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class RoomInfoInfoView(APIView):
     serializer_class = ChatRoomInfoSerializer
@@ -129,9 +163,9 @@ class RoomInfoInfoView(APIView):
             serializer = self.serializer_class(chat_room)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except ValidationError as e:
-            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
         except ChatRoom.DoesNotExist:
-            return Response({'status': 'not found'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"status": "not found"}, status=status.HTTP_404_NOT_FOUND)
 
 
 class DisconnectChatAPIView(APIView):
@@ -139,7 +173,7 @@ class DisconnectChatAPIView(APIView):
 
     def post(self, request, room_id):
         room = get_object_or_404(ChatRoom, room_id=room_id)
-        room.status = 'ENDED'
+        room.status = "ENDED"
         room.ended_at = timezone.now()
         room.save()
-        return Response('Disconnected', status=status.HTTP_200_OK)       
+        return Response("Disconnected", status=status.HTTP_200_OK)
